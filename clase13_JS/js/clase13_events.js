8a3a9329b8d5790f03457edb7dab3528175c6313
Eduardo Ramirez <leramirezt14@gmail.com>
2022-07-20 23:52:38 -0500

    clase13

---
+++ clase13_JS/js/clase13_events.js
@@ -0,0 +1,12 @@
+// ! EVENTOS
+
+    //! Estos suceden sobre los elementos de nuestro DOM
+    // y podemos capturar y reaccionar a ellos, manipularlos
+    // a nuestro favor
+
+
+    // TIPOS DE EVENTOS
+        //! Eventos Automaticos
+        //! Eventos generados por usuarios
+
+// ! EVENTOS EN LINEA -> Online events
